bb1373fd2ac2ba42e22412d7d8afebbe9a0095f0
Max Beneke <max.beneke@gmail.com>
2021-01-14 18:15:10 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    @@all = []
+
+    attr_reader :name
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.print_all
+        self.all.each {|dog| puts dog.name}
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+end
